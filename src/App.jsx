6023ac577afc9cf462318185d2f1974f29d62b96
scrat98 <artemgolovko98@gmail.com>
2018-06-12 17:26:44 +0300

    clean app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Twitter Demo</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
