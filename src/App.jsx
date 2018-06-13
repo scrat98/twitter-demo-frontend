4f7594901c543efc8b7b8db791a185e5f3cce61d
scrat98 <artemgolovko98@gmail.com>
2018-06-13 23:26:42 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Profile from './Profile';
 
 const App = () => (
-  <React.Fragment>
-    <h1>Twitter Demo</h1>
-  </React.Fragment>
+  <BrowserRouter>
+    <Switch>
+      <Redirect exact from="/" to="/EveryInteract" />
+      <Route path="/EveryInteract" component={Profile} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/Profile/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = () => (
+  <React.Fragment>
+    <h1>Profile</h1>
+  </React.Fragment>
+);
+
+export default Profile;
