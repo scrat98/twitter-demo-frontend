3bed769e7db2d38f6c44960ea5a5656cc4bc12cb
scrat98 <artemgolovko98@gmail.com>
2018-06-15 11:10:27 +0300

    add Tweets navigation

--- src/Profile/Tweets.jsx
+++ src/Profile/Tweets.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import Tweet from '../Tweet';
 
@@ -23,46 +24,96 @@ const TweetWrapper = styled.li`
   width: 100%;
 `;
 
+const NavigationList = styled.ul`
+  display: flex;
+  padding: 0 1rem;
+  border-bottom: 1px solid #e6ecf0;
+  background-color: #fff;
+  margin: 0;
+  list-style: none;
+`;
+
+const Navigation = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavigationLink = styled(NavLink)`
+  padding: 0.5rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: ${props => (props.activeClassName === 'active' ? 'black' : '#1da1f2')};
+
+  &:hover {
+    text-decoration: ${props =>
+      props.activeClassName === 'active' ? 'none' : 'underline'};
+  }
+`;
+
 export default () => (
-  <TweetsList>
-    <TweetWrapper>
-      <Tweet
-        context="Pinned Tweet"
-        contextImg={pinned}
-        userFullName="Every Interaction"
-        userNickName="EveryInteract"
-        userAvatar={avatar}
-        time="2 Mar 2015"
-        text="We’ve made some more resources for all you wonderful #design folk everyinteraction.com/resources/ #webdesign #UI"
-        img={tweet1}
-        replies={0}
-        retweets={17}
-        likes={47}
-      />
-    </TweetWrapper>
+  <React.Fragment>
+    <NavigationList>
+      <Navigation>
+        <NavigationLink exact to="/EveryInteract">
+          Tweets
+        </NavigationLink>
+      </Navigation>
+      <Navigation>
+        <NavigationLink exact to="/EveryInteract/with_replies">
+          Tweets & replies
+        </NavigationLink>
+      </Navigation>
+      <Navigation>
+        <NavigationLink exact to="/EveryInteract/media">
+          Media
+        </NavigationLink>
+      </Navigation>
+    </NavigationList>
+    <TweetsList>
+      <TweetWrapper>
+        <Tweet
+          context="Pinned Tweet"
+          contextImg={pinned}
+          userFullName="Every Interaction"
+          userNickName="EveryInteract"
+          userAvatar={avatar}
+          time="2 Mar 2015"
+          text="We’ve made some more resources for all you wonderful #design folk everyinteraction.com/resources/ #webdesign #UI"
+          img={tweet1}
+          retweets={17}
+          likes={47}
+        />
+      </TweetWrapper>
 
-    <TweetWrapper>
-      <Tweet
-        userFullName="Every Interaction"
-        userNickName="EveryInteract"
-        userAvatar={avatar}
-        time="23h"
-        text="Our new website concept; Taking you from… @ Every Interaction instagram.com/p/BNFGrfhBP3M/"
-        replies={1}
-        retweets={4}
-        likes={2}
-      />
-    </TweetWrapper>
+      <TweetWrapper>
+        <Tweet
+          userFullName="Every Interaction"
+          userNickName="EveryInteract"
+          userAvatar={avatar}
+          time="23h"
+          text="Our new website concept; Taking you from… @ Every Interaction instagram.com/p/BNFGrfhBP3M/"
+          replies={1}
+          retweets={4}
+          likes={2}
+        />
+      </TweetWrapper>
 
-    <TweetWrapper>
-      <Tweet
-        userFullName="Every Interaction"
-        userNickName="EveryInteract"
-        userAvatar={avatar}
-        time="Nov 18"
-        text="Variable web fonts are coming, and will open a world of opportunities for weight use online"
-        img={tweet3}
-      />
-    </TweetWrapper>
-  </TweetsList>
+      <TweetWrapper>
+        <Tweet
+          userFullName="Every Interaction"
+          userNickName="EveryInteract"
+          userAvatar={avatar}
+          time="Nov 18"
+          text="Variable web fonts are coming, and will open a world of opportunities for weight use online"
+          img={tweet3}
+        />
+      </TweetWrapper>
+    </TweetsList>
+  </React.Fragment>
 );
