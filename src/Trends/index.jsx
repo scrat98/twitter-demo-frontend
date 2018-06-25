689516030e0b0ecbe7a8c9c8e9087a04ac927944
scrat98 <artemgolovko98@gmail.com>
2018-06-25 22:39:01 +0300

    fix: change tweets to number

--- src/Trends/index.jsx
+++ src/Trends/index.jsx
@@ -9,28 +9,28 @@ const trends = [
   },
   {
     caption: '#FridayFeeling',
-    tweets: '12100',
+    tweets: 12100,
   },
   {
     caption: '#FridayFeeling',
-    tweets: '12100',
+    tweets: 12100,
   },
   {
     caption: '#BrexitAnniversary',
     description: 'It’s one year since the UK voted to leave the European Union',
-    tweets: '56700',
+    tweets: 56700,
   },
   {
     caption: 'HMS Queen Elizabeth',
-    tweets: '1036',
+    tweets: 1036,
   },
   {
     caption: 'Joe Budden',
-    tweets: '1036',
+    tweets: 1036,
   },
   {
     caption: 'Trident',
-    tweets: '6136',
+    tweets: 6136,
   },
 ];
 
